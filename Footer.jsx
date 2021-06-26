9be94a61975e4d06877b396d8f66bd18d104e45d
Sushanth UV <83573714+SushanthOO7@users.noreply.github.com>
2021-06-26 20:32:45 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by Sushanth UV @ {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+export default Footer;
